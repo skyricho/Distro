bbd12046b72e3397279dc1993bc13fb06bc6b873
Sky Richardson <skyricho@gmail.com>
2018-12-30 00:02:10 +1100

    Add filter parameter

--- index.php
+++ index.php
@@ -1,22 +1,37 @@
 <?php 
-include ("../dbaccess.php");
+include ("dbaccess.php");
 
+echo '<a href="?filter=all">All</a> | <a href="?filter=received">Received</a> | <a href="/distro">Not Received</a><br>';
 
-$request = $fm->newFindCommand('web');
-$request->addFindCriterion('status', '=='; 
-$request->addSortRule('firstNme');
-$result = $request->execute();
+if ($_GET['filter'] == 'all') {
+	$request = $fm->newFindAllCommand('web');
+    $result = $request->execute();
 
-if (FileMaker::isError($result)) {
-    echo "<p>Error: " . $result->getMessage() . "</p>"; exit;
+} elseif ($_GET['filter'] == 'received') {
+	$request = $fm->newFindCommand('web');
+	$request->addFindCriterion('status', 'received'); 
+	$request->addSortRule('firstName', 1);
+	$result = $request->execute();
+
+	if (FileMaker::isError($result)) {
+	    echo "<p>Error: " . $result->getMessage() . "</p>"; exit;
+	}
+
+} else {
+    $request = $fm->newFindCommand('web');
+    $request->addFindCriterion('status', '=='); 
+	$request->addSortRule('firstName', 1);
+	$result = $request->execute();
+
+	if (FileMaker::isError($result)) {
+	    echo "<p>Error: " . $result->getMessage() . "</p>"; exit;
+	}
+
 }
-//$request = $fm->newFindAllCommand('LayoutName');
-//$result = $request->execute();
-
 
 $records = $result->getRecords();
 foreach($records as $record) {
-    echo $record->getField('id') . ' ' . $record->getField('firstName') . '<br>';
+    echo $record->getField('firstName') . ' ' . $record->getField('status') . '<br>';
 }
 
 ?>
