f1763c38880d1aa3cc963802e95bac0598e13591
Sky Richardson <skyricho@gmail.com>
2018-12-30 18:56:04 +1100

    Add dropdown status filter

--- index.php
+++ index.php
@@ -6,9 +6,12 @@ $loader = new Twig_Loader_Filesystem('views');
 $twig = new Twig_Environment($loader);
 $template = $twig->load('index.html.twig');
 
+
+
 $foo = 'Hello World!';
 echo $template->render(array(
         'foo' => $foo,
+        'statusFilter' => $_GET['filter'],
         )
     );
 ?>
